e1e0ed7ffc52eb32f8b8bccb66d8ab00e7a42cbc
BlackRose484 <hungnbc2@gmail.com>
2024-11-05 07:52:22 +0700

    [backend][build] connect to cloud database

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_CLOUD_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
